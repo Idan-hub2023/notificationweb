afa6a7aec1adbc471fbea94d7804e73388676868
Idan-hub2023 <irahozadaniel9@gmail.com>
2024-11-14 17:06:08 +0200

    done4

--- home.php
+++ home.php
@@ -4,8 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style2.css">
 </head>
 <body>
-    <h1>welcome to our website</h1>
+<div class="contein">
+    <div class="top_nav">
+   <div class="log">
+    <h1>notification</h1>
+   </div>
+   <div class="links">
+    <a href="http://">Home</a>
+    <a href="http://">privet notificatio</a>
+    <a href="http://">puplic notificatio</a>
+    <a href="http://">news</a>
+   </div>
+      <!-- buttons -->
+      <input type="button" value="logout" name="logout" class="btns">
+</div>
+
+</div>
 </body>
 </html>
